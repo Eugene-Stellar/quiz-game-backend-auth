a7549a33d81054bea2958c0532c219db083b6f37
Eugene <bielichenko777eugene@gmail.com>
2026-02-24 14:42:12 +0100

    fix: add comment to clarify exclusion of UserDetailsServiceAutoConfiguration

--- src/main/java/eugenestellar/BackendAuthQuizApplication.java
+++ src/main/java/eugenestellar/BackendAuthQuizApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class}) // to shun a default password from SpringSecurity
 public class BackendAuthQuizApplication {
   public static void main(String[] args) {
     SpringApplication.run(BackendAuthQuizApplication.class, args);
